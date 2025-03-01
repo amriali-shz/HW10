42ef7f86e649af75df4615a81d12cc99bc1fd932
amirali-shz <business.a.shafizad@gmail.com>
2025-02-28 22:09:54 -0800

    the third is done!

--- src/ir/maktabsharif/q3/Application.java
+++ src/ir/maktabsharif/q3/Application.java
@@ -1,5 +1,55 @@
 package ir.maktabsharif.q3;
 
-public class Application {
+import java.util.*;
 
+public class Application {
+    public static void main(String[] args) {
+        String myInput = "11,1|1,5|5,4|4,7";
+        System.out.println(findLink(myInput));
+    }
+
+    private static String findLink(String input) {
+        List <int[]> pathList = new LinkedList<>();
+        Set<Integer> destination = new HashSet<>();
+        Set<Integer> mySource = new HashSet<>();
+
+        String[] pairs = input.split("\\|");
+        for (String pair : pairs) {
+            String[] numbers = pair.split(",");
+            int from = Integer.parseInt(numbers[0]);
+            int to = Integer.parseInt(numbers[1]);
+            pathList.add(new int[]{from, to});
+            mySource.add(from);
+            destination.add(to);
+        }
+        int start = -1;
+        for (int src : mySource) {
+            if (!destination.contains(src)) {
+                start = src;
+                break;
+            }
+        }
+        if (start == -1) {
+            return "invalid input";
+        }
+
+        LinkedList<Integer> sequence = new LinkedList<>();
+        sequence.add(start);
+
+        while (true) {
+            boolean found = false;
+            for (Iterator<int[]> iterator = pathList.iterator(); iterator.hasNext(); iterator.hasNext()) {
+                int[] pair = iterator.next();
+                if (pair[0] == start) {
+                    sequence.add(pair[1]);
+                    start = pair[1];
+                    iterator.remove();
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) break;
+        }
+        return String.join (" -> ", sequence.stream().map(String::valueOf).toList());
+    }
 }
